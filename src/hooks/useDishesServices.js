113092f1bbc5caf1179880efe9ee89f9da2f63db
Daniel <danielr2397@gmail.com>
2021-10-18 18:16:04 -0500

    Split custome hook dishes

---
+++ src/hooks/useDishesServices.js
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+
+import firebase from "firebase";
+import FireRequest from "../services/Request";
+
+export const useDishesServices = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  const [currentDish, setCurrentDish] = useState("");
+  const [show, setShow] = useState(false);
+  const [dish, setDish] = useState([]);
+  const handleShow = () => setShow(true);
+  const handleOpenAlert = () => setShowAlert(true);
+  const handleCloseAlert = (action) => setShowAlert(action);
+  const handleClose = () => {
+    setShow(false);
+    setTimeout(() => {
+      setCurrentDish("");
+    }, 0);
+  };
+
+  useEffect(() => {
+    getDishes();
+  }, []);
+
+  //Obtener platos de firebase
+  const getDishes = () => {
+    FireRequest()
+      .getServiceCondition(
+        "dishDocument",
+        "idRestaurant",
+        "==",
+        firebase.auth().currentUser.uid
+      )
+      .then((response) => {
+        const state = [];
+        response.forEach((doc) => {
+          state.push({
+            ...doc.data(),
+            id: doc.id,
+          });
+        });
+        setDish(state);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  //Crear o editar platos
+  const addDish = (object) => {
+    FireRequest()
+      .postService("dishDocument", {
+        ...object,
+        price: Number(object.price),
+      })
+      .then(() => console.log("Se cargó correctamente al documento"))
+      .catch((error) =>
+        console.error("Hubo un error al cargar en FireStore: ", error)
+      );
+    getDishes();
+  };
+
+  //Editar plato seleccionado
+  const editDish = (object) => {
+    FireRequest()
+      .updateService("dishDocument", currentDish, {
+        ...object,
+        price: Number(object.price),
+      })
+      .then(() => console.log("Se cargó correctamente al documento"))
+      .catch((error) =>
+        console.error("Hubo un error al cargar en FireStore: ", error)
+      );
+  };
+
+  //eliminar platos
+  const deleteDish = () => {
+    setShowAlert(false);
+    FireRequest()
+      .deleteService("dishDocument", currentDish)
+      .then(() => {
+        console.log("Document eliminado correctamente!");
+      })
+      .catch((error) => {
+        console.error("Error eliminando el plato: ", error);
+      });
+    setCurrentDish("");
+    getDishes();
+  };
+
+  return {
+    dish,
+    setCurrentDish,
+    handleShow,
+    show,
+    showAlert,
+    handleOpenAlert,
+    handleCloseAlert,
+    deleteDish,
+    currentDish,
+    addDish,
+    handleClose,
+  };
+};
--- src/pages/DishesPage.js
+++ src/pages/DishesPage.js
@@ -1,99 +1,30 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 
 import ModalDish from "../components/ModalDish";
 import AdditionalDish from "../components/AdditionalDish";
 import DeleteModal from "../components/DeleteModal";
 import { AuthContext } from "../context/Auth";
 
+import { useDishesServices } from "../hooks/useDishesServices";
+
 import { Edit, Add, HighlightOff } from "@material-ui/icons";
 import IconButton from "@material-ui/core/IconButton";
 
-import firebase from "firebase";
-import FireRequest from "../services/Request";
-
 function DishesPage() {
   const { currentUser } = useContext(AuthContext);
-  const [showAlert, setShowAlert] = useState(false);
-  const [currentDish, setCurrentDish] = useState("");
-  const [show, setShow] = useState(false);
-  const [dish, setDish] = useState([]);
-  const handleShow = () => setShow(true);
-  const handleOpenAlert = () => setShowAlert(true);
-  const handleCloseAlert = (action) => setShowAlert(action);
-  const handleClose = () => {
-    setShow(false);
-    setTimeout(() => {
-      setCurrentDish("");
-    }, 0);
-  };
-
-  useEffect(() => {
-    getDishes();
-  }, []);
-
-  //Obtener platos de firebase
-  const getDishes = () => {
-    FireRequest()
-      .getServiceCondition(
-        "dishDocument",
-        "idRestaurant",
-        "==",
-        firebase.auth().currentUser.uid
-      )
-      .then((response) => {
-        const state = [];
-        response.forEach((doc) => {
-          state.push({
-            ...doc.data(),
-            id: doc.id,
-          });
-        });
-        setDish(state);
-      })
-      .catch((e) => console.log(e));
-  };
-
-  //Crear o editar platos
-  const addDish = (object) => {
-    FireRequest()
-      .postService("dishDocument", {
-        ...object,
-        price: Number(object.price),
-      })
-      .then(() => console.log("Se cargó correctamente al documento"))
-      .catch((error) =>
-        console.error("Hubo un error al cargar en FireStore: ", error)
-      );
-    getDishes();
-  };
-
-  //Editar plato seleccionado
-  const editDish = (object) => {
-    FireRequest()
-      .updateService("dishDocument", currentDish, {
-        ...object,
-        price: Number(object.price),
-      })
-      .then(() => console.log("Se cargó correctamente al documento"))
-      .catch((error) =>
-        console.error("Hubo un error al cargar en FireStore: ", error)
-      );
-  };
-
-  //eliminar platos
-  const deleteDish = () => {
-    setShowAlert(false);
-    FireRequest()
-      .deleteService("dishDocument", currentDish)
-      .then(() => {
-        console.log("Document eliminado correctamente!");
-      })
-      .catch((error) => {
-        console.error("Error eliminando el plato: ", error);
-      });
-    setCurrentDish("");
-    getDishes();
-  };
+  const {
+    dish,
+    setCurrentDish,
+    handleShow,
+    show,
+    showAlert,
+    handleOpenAlert,
+    handleCloseAlert,
+    deleteDish,
+    currentDish,
+    addDish,
+    handleClose,
+  } = useDishesServices();
 
   //estructura de cards de platos
   const DishesCard = dish.map((todo, i) => {
