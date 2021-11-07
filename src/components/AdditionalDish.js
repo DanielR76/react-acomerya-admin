ce6199782c4e9727a62810615326706aa872610f
Daniel <danielr2397@gmail.com>
2021-11-06 21:09:50 -0500

    Add aditional service as hook

--- src/components/AdditionalDish.js
+++ src/components/AdditionalDish.js
@@ -1,99 +1,40 @@
 import React, { useState, useEffect } from "react";
+
+import { Edit, Delete } from "@mui/icons-material";
+import { TextField, IconButton } from "@mui/material";
+
 import DeleteModal from "../components/DeleteModal";
-import TextField from "@material-ui/core/TextField";
-import IconButton from "@material-ui/core/IconButton";
-import HighlightOffIcon from "@material-ui/icons/HighlightOff";
-import EditIcon from "@material-ui/icons/Edit";
 import IconMoney from "../assets/icon/iconos-moneda-01.svg";
-import firebase from "firebase";
-import { db } from "../utils/firebase";
+
+import { useAditionalDishService } from "../hooks/useAditionalDishService";
 import { useConvertValues } from "../hooks/useConvertValues";
 
 function AdditionalDish() {
   const [showAlert, setShowAlert] = useState(false);
-  const handleOpenAlert = () => setShowAlert(true);
-  const handleCloseAlert = (action) => setShowAlert(action);
   const [currentId, setCurrentId] = useState("");
   const [currentIdForm, setCurrentIdForm] = useState("");
   const [formAddition, setFormAddition] = useState(false);
   const { numberToCop } = useConvertValues();
+  const {
+    additionalValue,
+    listOfAdditions,
+    setAdditionalValue,
+    getAdditional,
+    getAdditionById,
+    addAditional,
+    editAditional,
+    deleteAddition,
+  } = useAditionalDishService();
 
-  const initialStateValues = {
-    idRestaurant: firebase.auth().currentUser.uid,
-    name: "",
-    price: "",
-  };
-
-  const [additionalValue, setAdditionalValue] = useState(initialStateValues);
-
-  //Obtener adiciones de firebase
-  const [additional, setAdditional] = useState([]);
-  const getAdditional = async () => {
-    db.collection("additionalDocument")
-      .where("idRestaurant", "==", firebase.auth().currentUser.uid)
-      .onSnapshot((querySnapshot) => {
-        const state = [];
-        querySnapshot.forEach((doc) => {
-          state.push({
-            ...doc.data(),
-            id: doc.id,
-          });
-        });
-        setAdditional(state);
-      });
-  };
   useEffect(() => {
     getAdditional();
   }, []);
 
-  //Obtener adicion por el id
-  const getAdditionById = async (id) => {
-    const doc = await db.collection("additionalDocument").doc(id).get();
-    setAdditionalValue({ ...doc.data() });
-    console.log(additionalValue);
-  };
+  const handleOpenAlert = () => setShowAlert(true);
 
-  //Crear o editar adiciones
-  const addOrEditDish = async (object) => {
-    if (currentIdForm === "") {
-      await db
-        .collection("additionalDocument")
-        .doc()
-        .set({ ...object, price: Number(object.price) })
-        .then(() => console.log("Se cargó correctamente al documento"))
-        .catch((error) =>
-          console.error("Hubo un error al cargar en FireStore: ", error)
-        );
-    } else {
-      await db
-        .collection("additionalDocument")
-        .doc(currentIdForm)
-        .update({ ...object, price: Number(object.price) })
-        .then(() => console.log("Se actualizó correctamente al documento"))
-        .catch((error) =>
-          console.error("Hubo un error al actualizar en FireStore: ", error)
-        );
-      setCurrentIdForm("");
-    }
-  };
+  const handleCloseAlert = (action) => setShowAlert(action);
 
-  //eliminar adiciones
-  const deleteAddition = async () => {
-    setShowAlert(false);
-    await db
-      .collection("additionalDocument")
-      .doc(currentId)
-      .delete()
-      .then(() => {
-        console.log("Document eliminado correctamente!");
-      })
-      .catch((error) => {
-        console.error("Error eliminando el plato: ", error);
-      });
-    setCurrentId("");
-  };
-
-  //Guardar los datos en values
+  //Save state values
   const handleChange = (e) => {
     setAdditionalValue({
       ...additionalValue,
@@ -101,18 +42,17 @@ function AdditionalDish() {
     });
   };
 
-  //Crear o editar adicion
+  //Create o edit aditional
   const handleSubmit = (e) => {
-    console.log(`se envia valor `);
     e.preventDefault();
-    addOrEditDish(additionalValue);
-    setAdditionalValue({ ...initialStateValues });
+    !currentIdForm ? addAditional() : editAditional(currentIdForm);
+    setCurrentIdForm("");
     setFormAddition(false);
   };
 
-  //Adicionales
-  const aditionalsCard = additional.map((val) => {
-    return (
+  //List of aditions
+  const AditionalsCard = () =>
+    listOfAdditions.map((val) => (
       <div className="container__add--aditions--values" key={val}>
         <div className="container__add--aditions--form">
           <div>
@@ -122,40 +62,37 @@ function AdditionalDish() {
           </div>
           <div className="price">
             <img src={IconMoney} alt="icon" />
-            <label className="container__add--price" key={`price ${val}`}>
+            <label className="container__add--price">
               {numberToCop(val.price)}
             </label>
           </div>
         </div>
         <div className="container__delete__edit--dish">
           <IconButton
-            key={`edit ${val}`}
             aria-label="editar"
             onClick={() => {
               setCurrentIdForm(val.id);
               getAdditionById(val.id);
             }}
           >
-            <EditIcon size="small" color="primary" />
+            <Edit size="small" color="primary" />
           </IconButton>
           <IconButton
-            key={`delete ${val}`}
             aria-label="eliminar"
             onClick={() => {
               setCurrentId(val.id);
               handleOpenAlert();
             }}
           >
-            <HighlightOffIcon size="small" color="secondary" />
+            <Delete size="small" color="secondary" />
           </IconButton>
         </div>
       </div>
-    );
-  });
+    ));
 
   return (
     <div className="container__form" key="1">
-      {aditionalsCard}
+      <AditionalsCard />
       {currentIdForm || formAddition ? (
         <form
           className="form"
@@ -205,7 +142,11 @@ function AdditionalDish() {
         name={"eliminar la adición del menú"}
         open={showAlert}
         close={handleCloseAlert}
-        delete={deleteAddition}
+        remove={() => {
+          setShowAlert(false);
+          deleteAddition(currentId);
+          setCurrentId("");
+        }}
       />
     </div>
   );
